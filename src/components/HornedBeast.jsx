c2cf4bc8ebb5377363bea70e0b47040e51b90a83
Immanuel Shin <Immanuel.shin18@gmail.com>
2023-11-28 19:27:40 +0000

    Added lab 02 feature #2

--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -1,18 +1,28 @@
-function HornedBeast ({
-    id,
-    title,
-    img,
-    description,
-    horns,
-    key,
+import { useState } from 'react';
+function HornedBeast({
+  id,
+  title,
+  img,
+  description,
+  horns,
+  key,
 }) {
-    return (
-        <figure className="horned-beast">
-            <h2>{title}</h2>
-            <img src={img} alt={title} title={title} />
-            <figcaption>{description}</figcaption>
-        </figure>
-    )
+  const [fav, setFav] = useState(0);
+
+  const favClick = () => {
+    setFav(fav + 1);
+  }
+
+  return (
+    <figure className="horned-beast">
+      <h2>{title}</h2>
+      <button onClick={favClick}>
+        <img src={img} alt={title} title={title} />
+      </button>
+      <figcaption>{description}</figcaption>
+      <figcaption>&#10084;: {fav}</figcaption>
+    </figure>
+  )
 }
 
 export default HornedBeast;
